be56da9b3cf95634bef0068f8481063302219b60
Jose Antonio <bladingnordeste@gmail.com>
2022-03-02 03:33:23 -0300

    curso em video + desafio

---
+++ ex5/index.js
@@ -0,0 +1,8 @@
+var vel = 80
+console.log(`A velocidade que seu carro atingiu foi de ${vel}Km/h `)
+
+if (vel > 60){   //condição simples!
+    
+    console.log('Voce ultrapassou a velociadade permitida, MULTADO!')
+}
+console.log(' dirija sempre com segunrança!')
---
+++ ex5/index2.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log(`vivendo em ${pais}`)
+if (pais == 'brasil'){
+    console.log('você é brasileiro')
+} else{
+    console.log('voce é estrangeiro')
+}
