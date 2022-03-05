c6c60e01b019c0544152b0297a2bbcde0d703b22
Jose Antonio <bladingnordeste@gmail.com>
2022-03-04 21:17:25 -0300

    curso em video

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,17 @@
-function verificar(){
-    var date = new Date()
-    var ano = date.getFullYear()
-    var fano = document.getElementById('textano')
-    var res = document. getElementById('res')
-    if(fano.value.length == 0 || Number (fano.value) > ano){
-        window.alert('Erro Tente novamente!!')
-    }
+var data = new Date()
+var ano = data.getFullYear()
+var mes = data.getMonth()
+var fano = document.getElementById ('txtano')
+var fmes = document.getElementById('txt1')
+var res = document.getElementById ('resul')
+var fsex = document.getElementsByTagName('radsex')
+
+   function verificar(){
+   if(fano.value.length == 0 || fano.value > ano){
+        window.alert('Erro, Verifique seus Dados!')
+   }else{
+       var idade = ano - fano.value
+       var meses = fmes.value - mes
+       res.innerHTML = `idade calculada ${idade} anos e ${meses} meses`
+   }
 }
