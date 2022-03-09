8e8faec0d87c580f99a9db8cfd2696ff8b9249f3
Jose Antonio <bladingnordeste@gmail.com>
2022-03-09 10:22:04 -0300

    Update script.js

--- praticando/formulario/script.js
+++ praticando/formulario/script.js
@@ -2,7 +2,7 @@ function verificar(){
 var nome = document.getElementById('name')
 var fidade = document.getElementById('idade')
 var res = document.getElementById('res')
-if(nome == undefined && fidade < 0){
+if(nome == 0 || fidade < 0){
     window.alert('[ERRO] Verifique suas informações e tente novamente!')
 }else if(fidade > 12){
     res.innerHTML = `Seja Bem vinda ${nome}`
