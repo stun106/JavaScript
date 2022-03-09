53704babb64b4e81ca5c8376535464db965c6cb5
Jose Antonio <bladingnordeste@gmail.com>
2022-03-09 10:19:52 -0300

    pratica

---
+++ praticando/formulario/script.js
@@ -0,0 +1,10 @@
+function verificar(){
+var nome = document.getElementById('name')
+var fidade = document.getElementById('idade')
+var res = document.getElementById('res')
+if(nome == undefined && fidade < 0){
+    window.alert('[ERRO] Verifique suas informações e tente novamente!')
+}else if(fidade > 12){
+    res.innerHTML = `Seja Bem vinda ${nome}`
+}
+}
