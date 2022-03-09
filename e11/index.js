7a5015e9a354cfe963a97c1de7de6444bf609b1b
Jose Antonio <bladingnordeste@gmail.com>
2022-03-09 15:51:18 -0300

    curso em video

---
+++ e11/index.js
@@ -0,0 +1,15 @@
+for(var int = 1;int<=10 ;int++){
+    console.log(int)
+}
+
+
+
+
+
+/*for(var int = 1; int <= 10;c++)   exemplo usando for*/
+
+/*var int = 1
+while(int <= 10){
+    console.log(int)
+    c++
+}*/
---
+++ ex10/index.js
@@ -0,0 +1,19 @@
+var int = 1
+while(int <= 10){
+console.log(`${int}`)
+int++
+}
+
+var int = 1
+do{
+    console.log(`${int}`)
+    int++
+}while(int <= 10){
+
+
+
+/*console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?')
+console.log('tudo bem ?') */
