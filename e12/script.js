b8a366af34368a6994e85e86f63c260273dd5875
Jose Antonio <bladingnordeste@gmail.com>
2022-03-11 10:03:57 -0300

    curso em video

--- e12/script.js
+++ e12/script.js
@@ -1,12 +1,12 @@
-
 function contador(){
 var inicio = document.getElementById('ini')
 var fim = document.getElementById('fim')
 var passo = document.getElementById('pas')
 var res = document.getElementById('res')
-var n1 = inicio.value
-var n2 = fim.value
-for(n1.value.length;n1<=n2;n1++){
-res.innerHTML = `${n1}`
+var n1 = Number(inicio.value)
+var n2 = Number(fim.value)
+var p = Number(passo.value)
+for(n1 = Number(inicio.value);n1<=n2;n1++ ){
+    res.innerHTML = `${n1}`
 }
 }
