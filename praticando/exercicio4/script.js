45ec7ccd432b60c536f4ce97c0239a830b514c63
Jose Antonio <bladingnordeste@gmail.com>
2022-03-24 06:15:15 -0300

    curso em video

--- praticando/exercicio4/script.js
+++ praticando/exercicio4/script.js
@@ -40,8 +40,41 @@ function inlister(n,l){
 
 function add(){
     if(innumber(num.value) && !inlister(num.value,valores)){
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        sel.appendChild(item)
+        res.innerHTML = ''
     }else{
         window.alert('Erro verifique seus dados')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finish(){
+    if (valores. length == 0){
+        window.alert('adicione um valor antes de finalizar!')
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+            maior = valores[pos]
+            if(valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todom temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p> o maior valore informado é ${maior}.</p>`
+        res.innerHTML += `<p> o menor valor informado é ${menor}.</p>`
+        res.innerHTML+= `<p> Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A media dos valores digitados é ${media}.</p>`
+
     }
 }
